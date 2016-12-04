45164cf92be681b9a648b8a9fe589d482ec3a4b2
joseegonzalez <mathcore@gmail.com>
2016-12-04 15:35:10 -0500

    intitial commit

---
+++ app/src/main/java/nyc/c4q/josegonzalez/easteregghunt/MainActivity.java
@@ -0,0 +1,13 @@
+package nyc.c4q.josegonzalez.easteregghunt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
