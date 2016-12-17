0a420d3c7fdd23b2265a40f59bcb7fb4859b5e6b
HakeemSackes-Bramble <hakksaxx@outlook.com>
2016-12-17 15:24:09 -0500

    adding insect class

---
+++ app/src/main/java/nyc/c4q/josegonzalez/easteregghunt/Insect.java
@@ -0,0 +1,47 @@
+package nyc.c4q.josegonzalez.easteregghunt;
+
+import java.util.Calendar;
+
+import nl.qbusict.cupboard.annotation.Column;
+
+/**
+ * Created by hakeemsackes-bramble on 12/17/16.
+ */
+
+public class Insect {
+
+    private Long _id;
+
+    private String name;
+
+    @Column("insect_url")
+    private Long timeAdded;
+
+
+    public Insect() {
+        this.name = "Unknown";
+        this.timeAdded = Calendar.getInstance().getTimeInMillis();
+    }
+
+    public Insect(String name, Long timeAdded) {
+        this.name = name;
+        this.timeAdded = timeAdded;
+    }
+
+    public Long getId() {
+        return _id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getTimeAdded() {
+        return timeAdded;
+    }
+}
+
